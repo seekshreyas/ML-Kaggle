0b2edc73841b145ae9479fd9bc7b84afd883db83
Shreyas <seekshreyas@gmail.com>
2013-12-11 18:02:09 -0800

    more cleanup

--- rforest.r
+++ rforest.r
@@ -1,6 +1,6 @@
 ## Kaggle Classifier
 ## ===================
-## Shreyas <shreyas@ischool.berkeley.edu>
+
 setwd("~/Documents/_Berkeley/sem3/stat154MachineLearning/ML-Kaggle")
 train_data <- read.table('data/zip_train.txt')
 test_data <- read.table('data/test_data.txt')
